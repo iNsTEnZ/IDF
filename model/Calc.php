6d5ffce0346fc58c59b4998779c5d030b527951a
osherre <oshry.1@gmail.com>
2017-06-05 21:02:38 +0300

    Create Calc.php
    Calc Math Expression
    3*3+2/1+3*6

---
+++ model/Calc.php
@@ -0,0 +1,19 @@
+<?php
+require_once("YahooApi.php");
+class calc extends YahooApi
+{
+  public function calcExpression($expr)
+  {
+	$url = "http://www.calcatraz.com/calculator/api?c=" . urlencode($expr);
+	
+    // Make call with cURL
+    $session = curl_init($url);
+    curl_setopt($session, CURLOPT_RETURNTRANSFER,true);
+    $json = curl_exec($session);
+    echo $json; 
+  }
+  public function getRoutingData()
+  {
+      return ['GET:api/calc' => function() { $this->calcExpression('"' . $_GET['expr'] . '"'); }];
+  }
+}
