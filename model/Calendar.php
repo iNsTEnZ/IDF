1e442b0cfaa0ea62b4be98b13d6ffcd7c8deed48
dekel <dekel.atzami@gmail.com>
2017-06-09 14:24:43 +0300

    changes to calendar

--- model/Calendar.php
+++ model/Calendar.php
@@ -3,53 +3,42 @@ namespace HolidayAPI;
 
 class Calendar
 {
-    private $parameters = array();
-
-    public function __set($variable, $value)
-    {
-        $this->parameters[$variable] = $value;
-    }
-
-    public function __construct($key = null)
-    {
-        if ($key) {
-            $this->key = $key;
-        }
-    }
+    private $parameters = ['key' => 'fb5d2526-979f-442e-ab8c-b8fe12c4627f'];
 
     public function holidays($parameters = array())
     {
         $parameters = array_merge($this->parameters, $parameters);
         $parameters = http_build_query($parameters);
 
-        $url  = 'https://holidayapi.com/v1/holidays?' . $parameters;
+        $url = 'https://holidayapi.com/v1/holidays?'. $parameters;
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL            => $url,
-            CURLOPT_HEADER         => false,
-            CURLOPT_SSL_VERIFYPEER => true,
-            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_URL             => $url,
+            CURLOPT_HEADER          => false,
+            CURLOPT_RETURNTRANSFER  => true,
         ));
 
         $response = curl_exec($curl);
 
-        if ($error = curl_error($curl)) {
-            return false;
+        if ($error = curl_error($curl)){
+            return $error;
         }
 
-        curl_close($curl);
-        $response = json_decode($response, true);
-
-        if (!$response) {
+        if (!$response){
             return false;
         }
 
         return $response;
+        $json = curl_exec();
+        echo $json;
     }
 
     public function getRoutingData()
     {
-      return null;
+        return ['GET:api/Calendar' => function() {$this->holidays('country');}];
     }
 }
+//$cal = new Calendar();
+//var_dump($cal -> holidays(['country'=>'IL', 'year'=>2017]));
+?>
