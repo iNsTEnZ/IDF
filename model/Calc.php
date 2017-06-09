db31c96dc7aca18cb7a0159e54440c9fc0fb76b7
osherre <oshry.1@gmail.com>
2017-06-09 13:32:50 +0300

    Create Calc.php

--- model/Calc.php
+++ model/Calc.php
@@ -7,25 +7,25 @@ class Calc extends YahooApi
   public function calcExpression($operator,$fvalue,$lvalue)
   {
 	$operator=$_REQUEST['operator'];
-	if($operator=="+")
+	if($operator=="plus")
 	{
 	$add1 = $fvalue;
 	$add2 = $lvalue;
 	$res= $add1+$add2;
 	}
-	if($operator=="-")
+	if($operator=="minus")
 	{
 	$add1 = $fvalue;
 	$add2 = $lvalue;
 	$res= $add1-$add2;
 	}
-	if($operator=="*")
+	if($operator=="mul")
 	{
 	$add1 = $fvalue;
 	$add2 = $lvalue;
 	$res =$add1*$add2;
 	}
-	if($operator=="/")
+	if($operator=="div")
 	{
 	$add1 = $fvalue;
 	$add2 = $lvalue;
